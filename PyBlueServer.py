3ade322179edd8cfe08ac0be659e36a4be8c8a42
Christian <christian.brissaud@ch4process.fr>
2017-02-21 10:23:21 +0100

    Init Echanges Data Client/serveur
    Programmes d'échanges de données entre deux appareils bluetooth en mode client serveur

---
+++ PyBlueServer.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+# programme de connexion avec appareil bluetooth
+# CB CH4Process
+##############################################################
+# programme pour reception data en provenance serveur Bluetooth
+# Bluetooth using PyBluez (with Python 2).
+#################################################################
+
+import bluetooth
+#  mettre a jour adresse device
+hostMACAddress = '00:1f:e1:dd:08:3d' # The MAC address of a Bluetooth adapter on the server. The server might have multiple Bluetooth adapters.
+port = 3
+backlog = 1
+size = 1024
+s = bluetooth.BluetoothSocket(bluetooth.RFCOMM)
+s.bind((hostMACAddress, port))
+s.listen(backlog)
+try:
+    client, clientInfo = s.accept()
+    while 1:
+        data = client.recv(size)
+        if data:
+            print(data)
+            client.send(data) # Echo back to client
+except:	
+    print("Closing socket")
+    client.close()
+    s.close()
---
+++ PyBluezClient.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+# programme de connexion avec appareil bluetooth
+# CB CH4Process
+##############################################################
+# programme pour envoi data evers client Bluetooth
+# Bluetooth using PyBluez (with Python 2).
+#################################################################PyBluez (with Python 2).
+
+
+import bluetooth
+#  mettre a jour adresse device
+serverMACAddress = '00:1f:e1:dd:08:3d'
+port = 3
+s = bluetooth.BluetoothSocket(bluetooth.RFCOMM)
+s.connect((serverMACAddress, port))
+while 1:
+    text = raw_input() # Note change to the old (Python 2) raw_input
+    if text == "quit":
+    break
+    s.send(text)
+sock.close()
